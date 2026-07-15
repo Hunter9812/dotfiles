9f2849c5f218d9f58118720f13c30693ff8082c5
Hunter Wang <hunter9812@qq.com>
2026-07-15 22:25:25 +0800

    efactor(nvim/autocmds): expand & organize 2-space indent filetypes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -4,7 +4,11 @@ local function set_tab2()
   vim.opt_local.softtabstop = 2
 end
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "sh", "bash", "zsh", "conf", "json", "jsonc", "sshconfig", "js", "md", "lua"},
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'markdown', 'lua', 'javascript', 'typescript', 'json', 'css',
+    'conf', 'dosini', 'jsonc', 'yaml', 'toml',
+    'sh', 'zsh', 'nu', 'sshconfig', 'gitconfig', 'dockerfile',
+  },
   callback = set_tab2,
 })
